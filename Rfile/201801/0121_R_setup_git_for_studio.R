7493cfe8cc3bb13402416d7fc9bce7a6cfb2e965
yuanqingye <yuan.qingye@yahoo.com>
2019-01-21 11:23:12 +0800

    Add new 0121 files

---
+++ Rfile/201801/0121_R_setup_git_for_studio.R
@@ -0,0 +1,4 @@
+#In Tools -> global Options -> GIT/SVN get git.exe path, something like:C:/Users/qingye.yuan/AppData/Local/GitHub/PortableGit_69bd5e6f85e4842f07db71c9618a621154c52254/bin/git.exe
+#if you don't have id_rsa.pub key; using file.exists("~/.ssh/id_rsa.pub") to check, then create one using rstudio.
+#copy your key to github website your account.
+#change settings in project Options fill in the current git repo path of your project there
